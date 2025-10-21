f77a6274947beee8c62c6717acd5c3615acc9061
Bruno Coulet <bruno.coulet@laplateforme.io>
2025-10-21 17:18:25 +0200

    palindrome - début

---
+++ poo/exercices/palindrome.py
@@ -0,0 +1,7 @@
+
+'''Un palindrome est une chaine de caractères qui peut se lire indifferment
+de gauche à droite et de droite à gauche'''
+class Palindrome:
+
+    def estPalindrome() ->bool:
+        pass
